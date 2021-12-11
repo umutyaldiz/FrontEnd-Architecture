c710c68b8d2852a4f09b43a7727c4c1af45fac4c
umutyaldiz <umutyaldiz95@gmail.com>
2021-12-11 15:02:50 +0300

    scroll Events added ++

--- src/js/components/scroll/index.js
+++ src/js/components/scroll/index.js
@@ -1,7 +1,19 @@
 export default class Scroll {
-    on() {
-        window.addEventListener('scroll', function () {
-            console.log('scrolling');
-        });
+    constructor(options) {
+
+    }
+    Event(OnCallback) {
+        var supportPageOffset = window.pageXOffset !== undefined;
+        var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+
+        var x = supportPageOffset ? window.pageXOffset : isCSS1Compat ? document.documentElement.scrollLeft : document.body.scrollLeft;
+        var y = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+
+        OnCallback({ x, y });
+    }
+    On(OnCallback, container = window) {
+        window.addEventListener('scroll', ()=>{
+            this.Event(OnCallback);
+        }, false);
     }
 }
--- src/js/pages/homepage/index.js
+++ src/js/pages/homepage/index.js
@@ -1,11 +1,26 @@
+import isInViewport from "../../components/isInViewport";
+import Scroll from "../../components/scroll";
 import Page from "../page";
+
 
 class homepage extends Page {
     constructor(options) {
         super(options);
     }
+    ScrollEvents(coordinate) {
+        //console.log("Page scroll Events", coordinate);
+
+        const text = isInViewport(document.querySelector("[category=highlights]")) ?
+            'category=highlights ekranda görünüyor' :
+            'category=highlights ekranda görünmüyor';
+
+        console.log(text);
+    }
     Init() {
         super.Load();
+
+        const scroll = new Scroll();
+        scroll.On(this.ScrollEvents);
     }
 }
 
