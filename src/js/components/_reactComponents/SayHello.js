c0c5f6069c1e1bf90b40c3d71713b6dfaa6a94ce
umutyaldiz <umutyaldiz95@gmail.com>
2021-12-13 16:21:58 +0300

    React Component Support ++

--- src/js/components/_reactComponents/SayHello.js
+++ src/js/components/_reactComponents/SayHello.js
@@ -15,8 +15,8 @@ export default class SayHello extends React.Component {
     render() {
         return (
             <div className='container my-4'>
-                <div className='block p-4 bg-white rounded-md shadow-md'>
-                    <div class="flex justify-between dark:bg-d-2 z-30">
+                <div className='block p-4 bg-white dark:bg-d-2 rounded-md shadow-md'>
+                    <div class="flex justify-between z-30">
                         <div class="flex items-center">
                             <h2 class="h-14 flex items-center font-black text-xl text-gray-700 dark:text-gray-300 dark:hover:text-white relative">
                                 <span class="absolute top-0 left-0 w-1/3 border-4 border-blue-600"></span>
